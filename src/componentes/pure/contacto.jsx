59094ba621c66339935ab372a0673867080ff183
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-06-14 23:11:25 -0400

    empece a crear contacto

---
+++ src/componentes/pure/contacto.jsx
@@ -0,0 +1,15 @@
+
+
+export class Contacto{
+    Name = "";
+    Apellido = "" ;
+    Email = "";
+    Conexion = false;
+    
+    constructor(name, Apellido, Email, Conexion){
+        this.name = name;
+        this.Apellido = Apellido;
+        this.Email = Email;
+        this.Conexion = Conexion;
+    }
+}
